f242b5f378bd1da0cfbaa15ab7b92d2080a48cd0
DevVain <InsaneVain786@gmail.com>
2024-08-07 07:16:04 +0100

    Initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TodoInput({ handleAddTodos, todoValue, setTodoValue }) {
+  
+
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter Task"
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("")
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
